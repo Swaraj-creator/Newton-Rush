d0d8e72e06b693af5ba33d674442da080f6eebc6
Swaraj Kashyap <swarajkumar7011@gmail.com>
2025-01-29 23:28:01 +0530

    Update game.js

--- game.js
+++ game.js
@@ -5,17 +5,42 @@ let pressedKeys = {
     ArrowLeft: false,
     ArrowRight: false
 }
-
+let player = {
+    started: true,
+    pos: 150
+}
 window.addEventListener("keydown", (e) => {
     pressed(e);
-});
+})
 window.addEventListener("keyup", (e) => {
     released(e);
 })
 
 function pressed(e) {
     pressedKeys[e.key] = true;
+    console.log(e.key);
 }
 function released(e) {
     pressedKeys[e.key] = false;
+    console.log(e.key);
 }
+
+function playGame() {
+    if(player.started) {
+        if(pressedKeys.ArrowLeft) {
+            if(player.pos-10 > 50) {
+                player.pos -= 10;
+            }
+        } if(pressedKeys.ArrowRight) {
+            if(player.pos+newton.clientWidth+10 < window.innerWidth - 50) {
+                player.pos += 10;
+            }
+        }
+        
+        newton.style.left = `${player.pos}px`;
+        
+        window.requestAnimationFrame(playGame);
+    }
+}
+
+window.requestAnimationFrame(playGame);
